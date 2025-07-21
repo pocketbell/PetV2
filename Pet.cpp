b48f2c35064b76dc4659068c810943d5dacc1309
Pocketbell <160898515+pocketbell@users.noreply.github.com>
2025-07-21 01:23:52 -0500

    Changed StringToState

--- Pet.cpp
+++ Pet.cpp
@@ -1,6 +1,7 @@
 #include "Pet.h"
 
-
+//Adjust Pet Values using Funct.h Functions
+//MaxValue, MinValue, RaiseValue(+=), LowerValue (-=), SetValue(=)
 void Pet::AdjustValue(Value value, void(*function)(int&, int), int modifier)
 {
 	switch (value)
@@ -13,14 +14,15 @@ void Pet::AdjustValue(Value value, void(*function)(int&, int), int modifier)
 	case Pet::Value::Str:			function(m_str,				modifier);	break;
 	case Pet::Value::Int:			function(m_int,				modifier);	break;
 	case Pet::Value::Sta:			function(m_sta,				modifier);	break;
-	case Pet::Value::MaxHealth:		function(m_maxHealth,			modifier);	break;
-	case Pet::Value::MaxEnergy:		function(m_maxEnergy,			modifier);	break;
+	case Pet::Value::MaxHealth:		function(m_maxHealth,		modifier);	break;
+	case Pet::Value::MaxEnergy:		function(m_maxEnergy,		modifier);	break;
 	case Pet::Value::Level:			function(m_level,			modifier);	break;
-	case Pet::Value::Experience:		function(m_experience,			modifier);	break;
-	case Pet::Value::ExperienceCap:		function(m_experienceCap, 		modifier);	break;
-	case Pet::Value::StatPoints:		function(m_statPoints,	  		modifier);	break;
+	case Pet::Value::Experience:	function(m_experience,		modifier);	break;
+	case Pet::Value::ExperienceCap:	function(m_experienceCap,   modifier);	break;
+	case Pet::Value::StatPoints:	function(m_statPoints,	    modifier);	break;
 	}
 }
+//Return Pet Values by Int
 int Pet::GetValue(Value value)
 {
 	switch (value)
@@ -34,10 +36,11 @@ int Pet::GetValue(Value value)
 	case Pet::Value::Int:				return m_int;
 	case Pet::Value::Sta:				return m_sta;
 	case Pet::Value::Level:				return m_level;
-	case Pet::Value::Experience:			return m_experience;
-	case Pet::Value::StatPoints:			return m_statPoints;
+	case Pet::Value::Experience:		return m_experience;
+	case Pet::Value::StatPoints:		return m_statPoints;
 	}
 }
+//Return Max Values by int
 int Pet::GetLimit(Limit limit)
 {
 	switch (limit)
@@ -46,65 +49,69 @@ int Pet::GetLimit(Limit limit)
 	case Pet::Limit::MinMood:			return m_MinMood;
 	case Pet::Limit::MaxHunger:			return m_MaxHunger;
 	case Pet::Limit::MinHunger:			return m_MinHunger;
-	case Pet::Limit::MaxHygiene:			return m_MaxHygiene;
-	case Pet::Limit::MinHygiene:			return m_MinHygiene;
+	case Pet::Limit::MaxHygiene:		return m_MaxHygiene;
+	case Pet::Limit::MinHygiene:		return m_MinHygiene;
 	case Pet::Limit::MaxHealth:			return m_maxHealth;
 	case Pet::Limit::MaxEnergy:			return m_maxEnergy;
-	case Pet::Limit::ExperienceCap:			return m_experienceCap;
+	case Pet::Limit::ExperienceCap:		return m_experienceCap;
 	}
 }
+//Auto Update Pet States based on set value range
 void Pet::SetMoodState()
 {
-	if	(m_mood > 80)		m_moodState = Pet::Mood::Happy;
+	if		(m_mood > 80)		m_moodState = Pet::Mood::Happy;
 	else if (m_mood > 60)		m_moodState = Pet::Mood::Okay;
 	else if (m_mood > 40)		m_moodState = Pet::Mood::Aggravated;
 	else if (m_mood > 0)		m_moodState = Pet::Mood::Sad;
 }
 void Pet::SetHungerState()
 {
-	if	(m_hunger > 80)		m_hungerState = Pet::Hunger::Full;
+	if		(m_hunger > 80)		m_hungerState = Pet::Hunger::Full;
 	else if (m_hunger > 60)		m_hungerState = Pet::Hunger::Fine;
 	else if (m_hunger > 40)		m_hungerState = Pet::Hunger::Hungry;
 	else if (m_hunger > 0)		m_hungerState = Pet::Hunger::Starving;
 }
 void Pet::SetHygieneState()
 {
-	if	(m_hygiene > 80)	m_hygieneState = Pet::Hygiene::Clean;
+	if		(m_hygiene > 80)	m_hygieneState = Pet::Hygiene::Clean;
 	else if (m_hygiene > 60)	m_hygieneState = Pet::Hygiene::Normal;
 	else if (m_hygiene > 40)	m_hygieneState = Pet::Hygiene::Dirty;
 	else if (m_hygiene > 0)		m_hygieneState = Pet::Hygiene::Filthy;
 }
+//Call all Pet State Updates
 void Pet::UpdateStates()
 {
 	SetMoodState();
 	SetHungerState();
 	SetHygieneState();
 }
+//Output current Pet States as String
 std::string Pet::StateToString(Pet::Value value)
 {
 	switch (value)
 	{
 	case Pet::Value::Mood:
-		if	(m_mood > 80)		 return "[Happy]";
-		else if (m_mood > 60)		 return "[Okay]";
-		else if (m_mood > 40)		 return "[Aggravated]";
-		else if (m_mood > 0)		 return "[Sad]";
-		break;						 
+		if		(m_moodState == Pet::Mood::Happy)				return "[Happy]";
+		else if (m_moodState == Pet::Mood::Okay)				return "[Okay]";
+		else if (m_moodState == Pet::Mood::Aggravated)			return "[Aggravated]";
+		else if (m_moodState == Pet::Mood::Sad)					return "[Sad]";
+		break;
 	case Pet::Value::Hunger:		 
-		if	(m_hunger > 80)		 return "[Full]";
-		else if (m_hunger > 60)		 return "[Fine]";
-		else if (m_hunger > 40)		 return "[Hungry]";
-		else if (m_hunger > 0)		 return "[Starving]";
+		if		(m_hungerState == Pet::Hunger::Full)			return "[Full]";
+		else if (m_hungerState == Pet::Hunger::Fine)			return "[Fine]";
+		else if (m_hungerState == Pet::Hunger::Hungry)			return "[Hungry]";
+		else if (m_hungerState == Pet::Hunger::Starving)		return "[Starving]";
 		break;
 	case Pet::Value::Hygiene:
-		if	(m_hygiene > 80)	 return "[Clean]";
-		else if (m_hygiene > 60)	 return "[Normal]";
-		else if (m_hygiene > 40)	 return "[Dirty]";
-		else if (m_hygiene > 0)		 return "[Filthy]";
+		if		(m_hygieneState == Pet::Hygiene::Clean)			return "[Clean]";
+		else if (m_hygieneState == Pet::Hygiene::Normal)		return "[Normal]";
+		else if (m_hygieneState == Pet::Hygiene::Dirty)			return "[Dirty]";
+		else if (m_hygieneState == Pet::Hygiene::Filthy)		return "[Filthy]";
 		break;
 	}
 	return "NoVal";
 }
+//Return States
 Pet::Mood Pet::GetMood() { return m_moodState; }
 Pet::Hunger Pet::GetHunger() { return m_hungerState; }
 Pet::Hygiene Pet::GetHygiene() { return m_hygieneState; }
